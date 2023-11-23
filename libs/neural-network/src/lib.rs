15b38c71339d419b6f60864b94e57966c43c0977
thinkdiffw <thinkdiffw@gmail.com>
2023-11-23 22:25:57 +0800

    init

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -9,6 +9,21 @@ impl Network {
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
+
+    pub fn random(layers: &[LayerTopology]) -> Self {
+        assert!(layers.len() > 1);
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(layers[0].neurons, layers[1].neurons))
+            .collect();
+
+        Self { layers }
+    }
+}
+
+pub struct LayerTopology {
+    pub neurons: usize,
 }
 
 // 分层
@@ -18,7 +33,17 @@ struct Layer {
 
 impl Layer {
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+
+    pub fn random(input_size: usize, output_size: usize) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::random(input_size))
+            .collect();
+        Self { neurons }
     }
 }
 
@@ -28,4 +53,26 @@ struct Neuron {
     bias: f32,
     // 输出权重
     weights: Vec<f32>,
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
+    }
+
+    pub fn random(input_size: usize) -> Self {
+        let bias = todo!();
+
+        let weights = (0..input_size).map(|_| todo!()).collect();
+
+        Self { bias, weights }
+    }
 }
